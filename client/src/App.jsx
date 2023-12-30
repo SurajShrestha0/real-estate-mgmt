a749eccdbee19b901c235e5f059e956b511d0438
SurajShrestha0 <np03cs4s220433@heraldcollege.edu.np>
2023-12-30 20:14:31 +0545

    Complete sign up UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Header from "./components/Header";
@@ -13,7 +13,7 @@ export default function App() {
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/sign-up" element={<SignOut />} />
+    <Route path="/sign-up" element={<SignUp />} />
     <Route path="/profile" element={<Profile />} />
     <Route path="/about" element={<About />} />
   </Routes>
