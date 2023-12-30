3c3d3fa25474c00baedc02200b0151a85b557900
SurajShrestha0 <np03cs4s220433@heraldcollege.edu.np>
2023-12-30 17:27:32 +0545

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -17,3 +18,12 @@ app.listen(3000, () =>{
     console.log('Server is running on port 3000');
 } 
 );
+
+app.get('/test', (req, res)=> {
+    res.send('Hello World');
+}
+);
+
+app.use('/api/user', userRouter);
+
+
