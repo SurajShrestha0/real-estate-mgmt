1eecbdc52ef4fa0869e31d50c646140647307ed3
SurajShrestha0 <np03cs4s220433@heraldcollege.edu.np>
2024-02-24 00:52:12 +0545

    update or edit listing fucntionality completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import About from "./pages/About";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 export default function App() {
   return (
@@ -27,6 +28,7 @@ export default function App() {
         <Route path="/broker-home" element={<BrokerHome />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/create-listing" element={<CreateListing />} />
+        <Route path="/update-listing/:listingId" element={<UpdateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
